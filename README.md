d136a7de90e4a11932683535f0a7b1f87e9d19a8
Kheops <arnault.nouvel@lgkev.com>
2022-05-17 23:18:35 +0900

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# Web3React - Talisman Integration
+
+This repository demonstrate how to integrate talisman with web3react.
+
+It is a copy of the [web3react v6 example project](https://github.com/NoahZinsmeister/web3-react/tree/v6/example), with the following changes :
+
+- added TalismanConnector which can be used the same way as web3react's InjectedConnector.
+- removed other connectors from the home page's connect options
+- removed the useEagerConnect hook that was built in the example, as it always forces Metamask to connect on page load, if authorized.
+- added a network switcher, which appears when a wallet is connected, to demonstrate how talisman networks can be switched from dapps.
+
+InjectedConnector uses any ethereum provider injected in `window.ethereum`, which most of the time should be Metamask.
+TalismanConnector uses exclusively Talisman ethereum provider which is injected in `window.talismanEth` instead. It is [EIP 1193](https://eips.ethereum.org/EIPS/eip-1193) compliant.
+We do not inject in `window.ethereum` unless it's empty, as we think most users will want to keep using both Talisman and Metamask.
+
+Please let us know if you need any assistance while integrating Talisman into your dapp.
+
+## Run the project
+
+```bash
+# install dependencies
+yarn
+
+# launch web server
+yarn start
+```
---
+++ pages/NetworkSwitcher.tsx
@@ -0,0 +1,73 @@
+import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from "@web3-react/core";
+import { useCallback } from "react";
+import { talismanChains } from "../talisman/talismanChains";
+
+export const NetworkSwitcher = () => {
+  const { chainId, library, account, active } = useWeb3React<Web3Provider>();
+
+  const switchNetwork = useCallback(
+    (network) => async () => {
+      try {
+        const res = await library?.send("wallet_switchEthereumChain", [
+          { chainId: "0x" + network.id.toString(16) },
+        ]);
+      } catch (err) {
+        console.log("failed to switch network", { err });
+        //attempt to add network in case it's missing from wallet
+        try {
+          const {
+            // non expected properties
+            name,
+            id,
+            testNet,
+            //expected properties
+            ...networkDetails
+          } = network;
+
+          await library?.send("wallet_addEthereumChain", [
+            {
+              chainId: "0x" + network.id.toString(16),
+              chainName: name,
+              ...networkDetails,
+            },
+          ]);
+        } catch (err) {
+          console.error("failed to add network", { err });
+        }
+      }
+    },
+    [library]
+  );
+
+  console.log("NetworkSwitcher", { chainId, library, account, active });
+
+  if (!library) return null;
+
+  return (
+    <div>
+      {talismanChains.map((network) => (
+        <button
+          key={network.id}
+          disabled={network.id === chainId}
+          onClick={switchNetwork(network)}
+        >
+          {network.name}
+        </button>
+      ))}
+      <p>
+        By default Talisman only supports Moonbeam, Moonriver, Astar, Acala and
+        their testnets.
+      </p>
+      <p>
+        You may add any other EVM network by toggling the "Allow custom
+        networks" in Talisman option. Toggling it off and on will clear the
+        custom networks list.
+      </p>
+      <p>
+        To reset authorized accounts for a given dapp, browse Talisman settings
+        - trusted sites - ethereum tab - and forget current site.{" "}
+      </p>
+    </div>
+  );
+};
